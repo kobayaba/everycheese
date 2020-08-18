a42bfa2fee05528ece00be8bad8137108cd10fff
kobayaba <yabakoba@gmai.com>
2020-08-18 00:01:31 +0000

    Ajout de l'application cheeses

---
+++ everycheese/cheeses/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from model_utils.models import TimeStampedModel
+from autoslug import AutoSlugField
+
+
+class Cheese(TimeStampedModel):
+    name = models.CharField('Name of Cheese', max_length=255)
+    slug = AutoSlugField('Cheese Address',
+        unique=True, always_update=False, populate_from='name')
+    description = models.TextField('Description', blank=True)
+
+    class Firmess(models.TextChoices):
+        UNSPECIFIED = 'unspecified', 'Unspecified'
+        SOFT = 'soft', 'Soft'
+        SEMI_SOFT = 'semi-soft', 'Semi-soft'
+        SEMI_HARD = 'semi-hard', 'Semi-hard'
+        HARD = 'hard', 'Hard'
+    
+    firmess = models.CharField('Firmess', max_length=20,
+        choices=Firmess.choices, default=Firmess.UNSPECIFIED)
+
+    def __str__(self):
+        return self.name
