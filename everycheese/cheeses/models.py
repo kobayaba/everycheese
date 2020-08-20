bd20015eb04d4e429550f512fcef1dc43008444d
kobayaba <yabakoba@gmai.com>
2020-08-20 14:56:49 +0000

    Ajouter du champ creator aux donnees preexistants

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from model_utils.models import TimeStampedModel
 from autoslug import AutoSlugField
 from django_countries.fields import CountryField
+from django.conf import settings
 
 
 class Cheese(TimeStampedModel):
@@ -11,6 +12,11 @@ class Cheese(TimeStampedModel):
         unique=True, always_update=False, populate_from='name')
     description = models.TextField('Description', blank=True)
     country_of_origin = CountryField("Pays d'origine", blank=True)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL
+    )
 
     class Firmess(models.TextChoices):
         UNSPECIFIED = 'unspecified', 'Unspecified'
