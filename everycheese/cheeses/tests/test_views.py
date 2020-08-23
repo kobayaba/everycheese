c9203e5b5c276c469dc14941e697660e80bc9a23
kobayaba <yabakoba@gmai.com>
2020-08-23 06:37:51 +0000

    Ecriture de test pour update

--- everycheese/cheeses/tests/test_views.py
+++ everycheese/cheeses/tests/test_views.py
@@ -13,9 +13,10 @@ from ..models import Cheese
 from ..views import (
     CheeseListView,
     CheeseDetailView,
-    CheeseCreateView
+    CheeseCreateView,
+    CheeseUpdateView
 )
-from .factories import CheeseFactory
+from .factories import CheeseFactory, cheese
 
 pytestmark = pytest.mark.django_db
 
@@ -24,8 +25,7 @@ def test_good_cheese_list_view(rf):
     response = CheeseListView.as_view()(request)
     assertContains(response, 'Cheese List')
 
-def test_good_cheese_detail_view(rf):
-    cheese = CheeseFactory()
+def test_good_cheese_detail_view(rf, cheese):
     url = reverse('cheeses:detail',
             kwargs={'slug':cheese.slug})
     request = rf.get(url)
@@ -48,8 +48,7 @@ def test_cheese_list_contains_2_cheeses(rf):
     assertContains(response, cheese1.name)
     assertContains(response, cheese2.name)
 
-def test_detail_contains_cheese_data(rf):
-    cheese = CheeseFactory()
+def test_detail_contains_cheese_data(rf, cheese):
     url = reverse('cheeses:detail',
         kwargs={'slug':cheese.slug})
     request = rf.get(url)
@@ -72,3 +71,33 @@ def test_cheese_create_form_valid(rf, admin_user):
     assert cheese.description == 'A salty hard cheese'
     assert cheese.firmess == Cheese.Firmess.HARD
     assert cheese.creator == admin_user
+
+def test_create_correct_title(rf, admin_user):
+    request = rf.get(reverse('cheeses:add'))
+    request.user = admin_user
+    response = CheeseCreateView.as_view()(request)
+    assertContains(response, 'Add Cheese')
+
+def test_good_cheese_update_view(rf, admin_user, cheese):
+    url = reverse('cheeses:update',
+        kwargs={'slug':cheese.slug})
+    request = rf.get(url)
+    request.user = admin_user
+    callable_obj = CheeseUpdateView.as_view()
+    response = callable_obj(request, slug=cheese.slug)
+    assertContains(response, 'Update Cheese')
+
+def test_cheese_update(rf, admin_user, cheese):
+    form_data = {
+        'name' : cheese.name,
+        'description': 'Something new',
+        'firmess': cheese.Firmess
+    }
+    url = reverse('cheeses:update',
+        kwargs={'slug':cheese.slug})
+    request = rf.post(url, form_data)
+    request.user = admin_user
+    callable_obj = CheeseUpdateView.as_view()
+    response = callable_obj(request, slug=cheese.slug)
+    cheese.refresh_from_db()
+    assert cheese.description == 'Something new'
